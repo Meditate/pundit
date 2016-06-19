fa41ca13c37e66cfe22af6769446f2aeaff3e960
Meditate <aroundofyou@gmail.com>
2016-06-19 23:53:53 +0200

    Add devise feature specs

---
+++ spec/features/user_sign_in_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'sign_in' do
+
+  let(:user) {create(:user)}
+
+  scenario 'user can not sign in if not registered' do
+    signin('invalid@example.com','invalidpass')
+    expect(page).to have_content('Invalid Email')
+  end
+
+  scenario 'user can sign in if valid credentials' do
+    signin(user.email,user.password)
+
+    expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario 'user can not sign in if invalid email' do
+    signin('invalidemail@example.com',user.password)
+
+    expect(page).to have_content 'Invalid Email'
+  end
+
+  scenario 'user can not sign in if invalid password' do
+    signin(user.email, 'invalidpassword')
+
+    expect(page).to have_content 'Invalid Email'
+  end
+
+end
